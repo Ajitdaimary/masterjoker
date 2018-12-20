4d22028cce13a5d3640151eb359edc15959e6660
badsahah <45558399+Ajitdaimary@users.noreply.github.com>
2018-12-20 14:02:39 +0530

    Update meme.py

--- meme.py
+++ meme.py
@@ -25,7 +25,17 @@ async def on_ready():
 	return await client.change_presence(game=discord.Game(name='Meme king | mk!help')) #add_your_bot_status_here
 
 
-
+@client.command(pass_context=True)
+async def tweet(ctx, usernamename:str, *, txt:str):
+    url = f"https://nekobot.xyz/api/imagegen?type=tweet&username={usernamename}&text={txt}"
+    async with aiohttp.ClientSession() as cs:
+        async with cs.get(url) as r:
+            res = await r.json()
+            r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+            embed = discord.Embed(color = discord.Color((r << 16) + (g << 8) + b))
+            embed.set_image(url=res['message'])
+            embed.title = "{} twitted: {}".format(usernamename, txt)
+            await client.say(embed=embed)
 
 
 
